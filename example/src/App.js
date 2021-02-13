e08fdb0c24911dd01da580bcdd9501041cd34147
SilverBirchh <bradleybirch09@yahoo.co.uk>
2021-02-13 09:14:54 +0000

    add useLazyGrid hook and example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useRef } from "react";
 
-import { useMyHook } from 'use-lazy-grid'
+import useLazyGrid from "use-lazy-grid";
+
+const items = new Array(10000).fill(0);
 
 const App = () => {
-  const example = useMyHook()
+  const parentRef = useRef(null);
+
+  const visible = useLazyGrid({
+    size: items.length,
+    gridRef: parentRef,
+    estimateSize: 100,
+  });
+
   return (
-    <div>
-      {example}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gap: 20,
+      }}
+      ref={parentRef}
+    >
+      {visible.map(({ index }) => {
+        return (
+          <div
+            key={index}
+            style={{
+              height: 100,
+              borderRadius: 5,
+              backgroundColor: "#F56565",
+            }}
+          />
+        );
+      })}
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,109 @@
-import * as React from 'react';
+import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+const minArray = [...Array(1).keys()].map((i) => ({ index: i }));
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
-
-  return counter;
+export type useLazyGridProps = {
+  size: number;
+  gridRef: React.RefObject<HTMLElement>;
+  estimateSize: number;
+  overscan: number;
 };
+
+export default function useLazyGrid({
+  size,
+  gridRef,
+  estimateSize,
+  overscan = 5,
+}: useLazyGridProps) {
+  const windowHeight = window.innerHeight;
+
+  const visibleRows = Math.floor(windowHeight / estimateSize) + overscan;
+
+  const [visibleAmount, setVisibleAmount] = useState(0);
+
+  const [childElementCount, setChildElementCount] = useState(0);
+
+  const initVisible = useRef(0);
+
+  useLayoutEffect(() => {
+    if (gridRef.current) {
+      const firstGridItem = gridRef.current.firstElementChild as HTMLElement;
+
+      const columnCount = Math.floor(
+        gridRef.current.offsetWidth / firstGridItem?.offsetWidth
+      );
+
+      const visibleItems = Number.isNaN(columnCount)
+        ? 1
+        : visibleRows * columnCount;
+
+      initVisible.current = visibleItems;
+
+      setVisibleAmount(Math.min(size, visibleItems));
+    }
+  }, [gridRef, visibleRows, size, gridRef.current?.firstElementChild]);
+
+  const loadMore = useCallback(() => {
+    setVisibleAmount((visibleAmount) => {
+      const newVisible = visibleAmount + initVisible.current;
+
+      if (newVisible >= size) {
+        return size;
+      }
+
+      return visibleAmount + initVisible.current;
+    });
+  }, [size]);
+
+  useLayoutEffect(() => {
+    if (gridRef.current) {
+      const lastGridItem = gridRef.current.lastElementChild;
+
+      if (lastGridItem) {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                loadMore();
+              }
+            });
+          },
+          {
+            rootMargin: "50px 50px 50px 50px",
+          }
+        );
+
+        observer.observe(lastGridItem);
+
+        return () => observer.unobserve(lastGridItem);
+      }
+    }
+    return undefined;
+  }, [gridRef, childElementCount, loadMore]);
+
+  useLayoutEffect(() => {
+    if (gridRef.current) {
+      const config = { childList: true, subtree: true };
+
+      const callback: MutationCallback = function (mutationsList) {
+        for (const mutation of mutationsList) {
+          if (mutation.type === "childList") {
+            const element = mutation.target as HTMLElement;
+            setChildElementCount(element.childElementCount);
+          }
+        }
+      };
+
+      const observer = new MutationObserver(callback);
+
+      observer.observe(gridRef.current, config);
+
+      return () => observer.disconnect();
+    }
+    return undefined;
+  }, [gridRef, gridRef.current]);
+
+  return size < visibleAmount
+    ? minArray
+    : [...Array(visibleAmount).keys()].map((i) => ({ index: i }));
+}
